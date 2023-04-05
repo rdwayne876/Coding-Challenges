dce5ce247ede24960267971625bc050b257c6c53
Dwayne Robinson <rdwayne876@gmail.com>
2023-04-05 07:11:35 -0500

    List Filtering

---
+++ index.js
@@ -0,0 +1,20 @@
+import { filter_list } from "./challenges/ListFiltering.js";
+import { solution } from "./challenges/Multiples3or5.js";
+
+/**
+DESCRIPTION:
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+solution(10);
+
+/**
+DESCRIPTION:
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+*/
+
+filter_list([1,2,'aasf','1','123',123])
